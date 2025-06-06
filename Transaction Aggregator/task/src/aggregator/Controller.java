cb68d2b6b54f2f12f46ddd36a9a99525bad25a2a
LIR-56 <lir-567@rambler.ru>
2025-06-06 12:30:44 +0200

    Stage 3

--- Transaction Aggregator/task/src/aggregator/Controller.java
+++ Transaction Aggregator/task/src/aggregator/Controller.java
@@ -2,11 +2,13 @@ package aggregator;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -17,21 +19,36 @@ public class Controller {
 
     @RequestMapping("/aggregate")
     public ArrayList<Transaction> aggregate(@RequestParam @NonNull String account) {
-        RestTemplate restTemplate = new RestTemplate();
 
         String url1 = "http://localhost:8888/transactions?account=" + account;
         String url2 = "http://localhost:8889/transactions?account=" + account;
 
-        ResponseEntity<List<Transaction>> response1 =
-                restTemplate.exchange(url1, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
-        ResponseEntity<List<Transaction>> response2 =
-                restTemplate.exchange(url2, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+
         var result = new ArrayList<Transaction>();
-        result.addAll(response1.getBody());
-        result.addAll(response2.getBody());
+        result.addAll(requestAndRetryFiveTimes(url1));
+        result.addAll(requestAndRetryFiveTimes(url2));
         result.sort((t1, t2) -> -1 * t1.timestamp.compareTo(t2.timestamp));
         return result;
     }
 
-    public record Transaction(String id, String serverId, String account, String amount, String timestamp) {}
+    private List<Transaction> requestAndRetryFiveTimes(String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        int i = 0;
+        while (i < 5) {
+            ResponseEntity<List<Transaction>> response;
+            try {
+                response =
+                        restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+                if (response.getStatusCode() == HttpStatusCode.valueOf(200)){
+                    return response.getBody();
+                }
+            } catch (RestClientException ignored) {
+                i++;
+            }
+        }
+        return List.of();
+    }
+
+    public record Transaction(String id, String serverId, String account, String amount, String timestamp) {
+    }
 }
